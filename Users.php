43caf204df4d478b52900072c572a98028c8afb5
work <work@greta.ebaby.objectivemoon.io>
2017-03-04 17:15:02 +0100

    Users Function Ok

--- Users.php
+++ Users.php
@@ -3,11 +3,11 @@
 function get_mail_user($id_user){
 	include "connexion.php";
 
-	$select = "SELECT mail FROM users WHERE id_user = $id_user";
+	$select = "SELECT mail FROM users WHERE id_user = '$id_user'";
 	$result = pg_query($dbconnect, $select);
-	$mail = pg_fetch_all($result);
+	$mail = pg_fetch_row($result);
 
-	return $mail;
+	return $mail[0];
 }
 
 function get_lastname_user($id_user){
@@ -17,7 +17,7 @@ function get_lastname_user($id_user){
 	$result = pg_query($dbconnect, $select);
 	$lastname = pg_fetch_row($result);
 
-	return $lastname;
+	return $lastname[0];
 }
 
 
@@ -28,7 +28,7 @@ function get_firstname_user($id_user){
 	$result = pg_query($dbconnect, $select);
 	$firstname = pg_fetch_row($result);
 
-	return $firstname;
+	return $firstname[0];
 }
 
 function get_password_user($id_user){
@@ -38,18 +38,21 @@ function get_password_user($id_user){
 	$result = pg_query($dbconnect, $select);
 	$password = pg_fetch_row($result);
 
-	return $password;
+	return $password[0];
 }
 
 function insert_user($lastname, $firstname, $mail, $password){
 	include 'connexion.php';
 
-	$date_inscription = new Date()//function to transform date if doesnt work
-	$pass = ;//Hash password
+	$date = new DateTime;
+	$date_inscription = $date->format('d-m-Y');
+	$pass = hash('whirlpool', $date_inscription.$password);
 
 
-	$insert = "INSERT INTO users (lastname, firstname, mail, password, date_inscription, upvote) VALUES ($lastname, $firstname, $mail, $pass_hash, $date_inscription, 0)";
+	$insert = "INSERT INTO users (lastname, firstname, mail, password, date_inscription, upvote) VALUES ('$lastname', '$firstname', '$mail', '$pass', '$date_inscription', '0')";
 
 	$result = pg_query($dbconnect, $insert);
+
+	return $insert;
 }
 
--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 
 include "connexion.php";
 include "Questions.php";
+include "Users.php";
 
-
-print_r(get_nb_upvote_question(2));
+print_r(get_password_user(5));
 
 
 /*
+Question and upvote:
 
 insert_question OK
 insert_upvote OK miss insert upvote to user
@@ -17,3 +18,9 @@ get_question OK
 get_nb_upvote_question OK
 
 */
+
+/*
+
+Users:
+insert_user OK
+get_mail_user OK
