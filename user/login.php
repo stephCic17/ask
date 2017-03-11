804a269eac6d0639401993e0110b47d39de8b14e
work <work@greta.ebaby.objectivemoon.io>
2017-03-11 15:46:42 +0100

    logout ok

--- user/login.php
+++ user/login.php
@@ -17,11 +17,11 @@ if (test_pseudo($pseudo) == 1){
 		echo "<script> document.location.href=\"http://ciconia.io/ask\"</script>";
 	}
 	else{
-		echo "KOici";
+		echo "<script> document.location.href=\"http://ciconia.io/ask\"</script>";
 	}
 }
 	else
-		echo "KO";
+		echo "<script> document.location.href=\"http://ciconia.io/ask\"</script>";
 	
 
 ?>
