7dc9727d9ec43a8bea46b0e881db085eb52a7e09
work <work@ciconia.io>
2017-03-15 22:22:35 +0100

    add separate css

---
+++ function.php
@@ -0,0 +1,11 @@
+<?php
+
+function get_footer(){
+	return "<footer id='footer'>
+<ul>
+<p>contact@ciconia.io<p>
+<li><a id='fb-link'><i class='icon -facebook'></i></a></li>
+<li><a id='link-legal' class='uppercase center' href='./legal.php'>Mention légale</a></li>
+</ul>
+</nav>";
+}
