8c663cae2419b7235deb0acc448ca964c0a81d15
tfrere <tfrere@student.42.fr>
2017-03-16 23:58:09 +0100

    update loginModal

--- user/popup.js
+++ user/popup.js
@@ -1,26 +1,42 @@
-$(".open-login-modal").click(function() {
-	$("#loginModal").addClass("-opening");
-	window.setTimeout(function() {
-		$("body").addClass("no-scroll");
-		$("#loginModal").addClass("-open");
-		$("#loginModal").removeClass("-opening");
-	}, 600);
-});
+$(document).ready(function(){
 
-$(".open-subscribe-modal").click(function() {
-	$("#subscribeModal").addClass("-opening");
-	window.setTimeout(function() {
-		$("body").addClass("no-scroll");
-		$("#subscribeModal").addClass("-open");
-		$("#subscribeModal").removeClass("-opening");
-	}, 600);
-});
+	$(".open-login-modal").click(function() {
 
-$(".close-modal").click(function() {
-	$(".modal").addClass("-closing");
-	window.setTimeout(function() {
-		$(".modal").removeClass("-open");
-		$(".modal").removeClass("-closing");
-		$("body").removeClass("no-scroll");
-	}, 350);
+		$("#loginModal").addClass("-opening");
+		window.setTimeout(function() {
+
+			$("body").addClass("no-scroll");
+			$("#loginModal").addClass("-open");
+			$("#loginModal").removeClass("-opening");
+
+		}, 600);
+
+	});
+
+	$(".open-subscribe-modal").click(function() {
+
+		$("#subscribeModal").addClass("-opening");
+		window.setTimeout(function() {
+
+			$("body").addClass("no-scroll");
+			$("#subscribeModal").addClass("-open");
+			$("#subscribeModal").removeClass("-opening");
+
+		}, 600);
+
+	});
+
+	$(".close-modal").click(function() {
+
+		$(".modal").addClass("-closing");
+		window.setTimeout(function() {
+
+			$(".modal").removeClass("-open");
+			$(".modal").removeClass("-closing");
+			$("body").removeClass("no-scroll");
+
+		}, 350);
+
+	});
+
 });
