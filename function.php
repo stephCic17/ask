55e6097f4a088d0d88f9649d6bcc161b1f9287ff
tfrere <tfrere@student.42.fr>
2017-03-17 01:10:56 +0100

    update loginModal

--- function.php
+++ function.php
@@ -1,11 +1,35 @@
 <?php
 
 function get_footer(){
-	return "<footer id='footer'>
-<ul>
-<p>contact@ciconia.io<p>
-<li><a id='fb-link'><i class='icon -facebook'></i></a></li>
-<li><a id='link-legal' class='uppercase center' href='./legal.php'>Mention légale</a></li>
-</ul>
-</nav>";
+	return '
+	<footer id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="three col">
+					<div id="logo"></div>
+				</div>
+				<div class="three col">
+					<h6>Réseaux sociaux</h6>
+					<ul>
+						<li><a href="#">Google +</a></li>
+						<li><a href="http://twitter.com/peon_actu">Twitter</a></li>
+						<li><a href="http://facebook.com/gopeon">Facebook</a></li>
+					</ul>
+				</div>
+				<div class="three col">
+					<h6>Presse</h6>
+					<ul>
+						<li><a href="#"></a>Contactez-nous</li>
+						<li><a href="#"></a></li>
+					</ul>
+				</div>
+				<div class="three col">
+					<h6>Légal</h6>
+					<ul>
+						<li><a href="#">Mentions-légales</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>';
 }
