f62bb825f2a6fbb857e923930d1eefc8e21122a6
tfrere <tfrere@student.42.fr>
2017-03-17 15:53:46 +0100

    updateQuestion

--- question/questionAjax.php
+++ question/questionAjax.php
@@ -8,20 +8,18 @@ session_start();
 $d = array();
 extract ($_POST);
 
-if ($_POST["action"] == "addQuestion"){
+if ($_POST["action"] == "addQuestion") {
 	$d["test"] = insert_question($_SESSION["id"], htmlentities($question, ENT_QUOTES, "UTF-8"), 1);
 	$d["erreur"] = "ok";
 }
 
-if ($_POST["action"] == "addUpvote")
-{
-	if ($_SESSION["id"])
-		{
+if ($_POST["action"] == "addUpvote") {
+	if ($_SESSION["id"]) {
 			$id_user = $_SESSION["id"];
 			$select = "SELECT id_up FROM upvotes WHERE id_q='$id' AND id_user='$id_user'";
 			$result = pg_query($dbconnect, $select);
 			$res = pg_fetch_row($result);
-			if (!$res[0]){
+			if (!$res[0]) {
 				$d["test"] = insert_upvote($_SESSION["id"],$id);
 				$d["erreur"] = "ok";
 				$question = get_all_question(1);
@@ -32,12 +30,12 @@ if ($_POST["action"] == "addUpvote")
 				}
 			}
 		}
-		else{
+		else {
 			$d["erreur"] = "id";
 		}
 }
 
-if ($_POST["action"] == "getQuestions"){
+if ($_POST["action"] == "getQuestions") {
 	$d["erreur"] = "ok";
 	$d["lastid"] = get_last_question();
 	$question = get_all_question_answer(1);
